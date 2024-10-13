7227f2712f68c1cfdb6f71d80c424c9c2c13d867
mohsinstar <mouhcinetalbi@gmail.com>
2024-10-13 03:32:33 +0100

    google analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,25 @@
 /// <reference path="../.astro/types.d.ts" />
 declare module 'nodemailer';
+
+// Déclarations pour Google Analytics
+interface Window {
+  dataLayer: any[];
+  gtag: (...args: any[]) => void;
+}
+
+// Déclaration pour Axeptio
+interface Window {
+  axeptioSettings: {
+    clientId: string;
+    cookiesVersion: string;
+    googleConsentMode: {
+      default: {
+        analytics_storage: string;
+        ad_storage: string;
+        ad_user_data: string;
+        ad_personalization: string;
+        wait_for_update: number;
+      }
+    }
+  };
+}
